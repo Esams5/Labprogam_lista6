d87c3fd0ce12bb6a4217fd8f70bebe2e542e6341
Samuel Esdras <118021278+Esams5@users.noreply.github.com>
2023-06-26 03:29:56 -0300

    Update q4.c

--- q4.c
+++ q4.c
@@ -1,25 +1,43 @@
 #include <stdio.h>
 
-struct aluno{
-    float nota[2];
+#define TAM 3
+
+struct Aluno{
+    float n1;
+    float n2;
     float media;
 };
 
-int main(int argc,char **argv){
-    struct aluno a[3];
+struct Aluno calcMedia2(struct Aluno);
 
-    for(int i=0;i<3;i++){
-        a[i].nota[0]=rand()%11;
-        a[i].nota[1]=rand()%11;
+int main(){
+
+    struct Aluno als[TAM];
+
+    // Entrada de Dados
+    for (int k=0; k<TAM; k++){
+        printf("Digite a primeira e a 2 nota do aluno %d\n: ",k+1);
+        scanf("%f",&als[k].n1);
+        scanf("%f",&als[k].n2);
     }
 
-    for(int i=0;i<3;i++){
-        calc_media(&a[i]);
+    // Processamento
+    for (int k=0; k<TAM; k++){
+        als[k] = calcMedia2(als[k]);
+    }
+    
+    // Saída
+    for (int k=0; k<TAM; k++){
+        printf("Média do aluno %d: %.2f\n",k+1, als[k].media);
     }
 
     return 0;
+
 }
 
-void calc_media(struct aluno *a){
-    a->media = (a->nota[0]+a->nota[1])/2;
+struct Aluno calcMedia2(struct Aluno a){
+    a.media = (a.n1 + a.n2) / 2;
+
+    return a;
+
 }
